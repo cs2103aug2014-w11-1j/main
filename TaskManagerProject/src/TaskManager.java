08249c36fc8f0ca64bf5eee0eddb0fbd693830a4
ohohcakester <ohohgit@gmail.com>
2014-08-27 06:46:16 +0800

    initial commit

---
+++ TaskManagerProject/src/TaskManager.java
@@ -0,0 +1,9 @@
+
+public class TaskManager {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I am a task manager!");
+        System.out.println("I manage tasks!");
+        System.out.println("You have no tasks to do today!");
+    }
+}
