ddf7126f40f5a8f83f9e9709cbe6ba626f03f48f
dingming1994 <a0119432@nus.edu.sg>
2014-09-24 18:16:12 +0800

    Add state check for state manager

--- TaskManagerProject/src/manager/StateManager.java
+++ TaskManagerProject/src/manager/StateManager.java
@@ -5,32 +5,65 @@ import manager.datamanager.UndoManager;
 import manager.result.Result;
 
 public class StateManager {
+	
     private final UndoManager undoManager;
+    
+    // Availability of command. True for able, false for unable
+    private boolean addCheck;
+    private boolean deleteCheck;
+    private boolean searchCheck;
+    private boolean undoCheck;
+    private boolean editCheck;
+ 
 
     public StateManager(UndoManager undoManager) {
         this.undoManager = undoManager;
+        addCheck = true;
+        deleteCheck = true;
+        searchCheck = true;
+        undoCheck = true;
+        editCheck = true;
     }
 
     public boolean canAdd() {
-        throw new UnsupportedOperationException("Not Implemented Yet");
+    	return addCheck;
     }
     
     public boolean canSearch() {
-        throw new UnsupportedOperationException("Not Implemented Yet");
+    	return searchCheck;
     }
     
     public boolean canEdit() {
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        return editCheck;
     }
     
     public boolean canDelete() {
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        return deleteCheck;
     }
 
     public boolean canUndo() {
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        return undoCheck;
     }
     
+    public void enterAddMode(){
+    	addCheck = false;
+    }
+    
+    public void enterSearchMode(){
+    	searchCheck = false;
+    }
+    
+    public void enterEditMode(){
+    	editCheck = false;
+    }
+    
+    public void enterDeleteMode(){
+    	deleteCheck = false;
+    }
+    
+    public void enterUndoMode(){
+    	undoCheck = false;
+    }
     /**
      * Updates the program's state using the result obtained from the managers.
      * @param result
