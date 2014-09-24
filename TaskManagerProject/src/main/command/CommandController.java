0936a967dfb368a96102e1d36ee9b833a37a24b9
YJSoh <soh.you.jun@gmail.com>
2014-09-24 16:13:45 +0800

    Add partial functionality to the command controller class.

--- TaskManagerProject/src/main/command/CommandController.java
+++ TaskManagerProject/src/main/command/CommandController.java
@@ -1,11 +1,44 @@
 package main.command;
 
+import java.util.Scanner;
+
+import manager.ManagerHolder;
+
 /**
  * Handles the parsing of commands.
  * Commands are returned to MainController for execution.
- * 
+ *
  * @author You Jun
  */
 public class CommandController {
-    
+    private ManagerHolder managerHolder;
+
+    public CommandController(ManagerHolder managerHolder) {
+        this.managerHolder = managerHolder;
+    }
+
+    public Command getCommand(String cmdTxt) {
+        Scanner sc = new Scanner(cmdTxt);
+
+        String cmdType = sc.next();
+        String cmdArgs = sc.nextLine().trim();
+
+        sc.close();
+
+        return getCommand(cmdType, cmdArgs);
+    }
+
+    private Command getCommand(String cmdType, String cmdArgs) {
+        switch (cmdType) {
+            case "add":
+                return new AddCommand(cmdArgs, managerHolder);
+            case "show":
+                //return new SearchCommand(cmdArgs);
+            case "edit":
+                //return new EditCommand(cmdArgs);
+            case "del":
+                //return new DeleteCommand(cmdArgs);
+        }
+        return null;
+    }
 }
