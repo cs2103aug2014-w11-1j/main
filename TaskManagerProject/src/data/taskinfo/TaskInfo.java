2e485e069fe889687f3cecb265414bc75a7891e7
YJSoh <soh.you.jun@gmail.com>
2014-09-24 21:20:54 +0800

    Changed TaskInfo to use LocalDate and LocalTime for the time and dates. Duration for task duration and repeat interval.

--- TaskManagerProject/src/data/taskinfo/TaskInfo.java
+++ TaskManagerProject/src/data/taskinfo/TaskInfo.java
@@ -1,45 +1,49 @@
 package data.taskinfo;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
-import java.util.Date;
 
 public class TaskInfo {
 
     public String name;
-    public Time startTime;
-    public Time endTime;
-    public Date date;
+    public Duration duration;
+    public LocalTime endTime;
+    public LocalDate endDate;
+    public Duration repeatInterval;
     public String details;
     public Tag[] tags;
     public Priority priority;
     public Status status;
-    
+
     public int numberOfTimes;
     public int repeatIntervalDays;
 
     public static final int REPEAT_INDEFINITELY = -1;
-    
+
     public TaskInfo() {
     }
-    
+
     /**
      * Copy constructor
      * @param taskInfo taskInfo to copy.
      */
     public TaskInfo(TaskInfo taskInfo) {
         this.name = taskInfo.name;
-        this.startTime = taskInfo.startTime;
+        this.duration = taskInfo.duration;
         this.endTime = taskInfo.endTime;
-        this.date = taskInfo.date;
+        this.endDate = taskInfo.endDate;
         this.details = taskInfo.details;
-        
-        if (taskInfo.tags != null)
+
+        if (taskInfo.tags != null) {
             this.tags = Arrays.copyOf(taskInfo.tags, taskInfo.tags.length);
-        
+        }
+
         this.priority = taskInfo.priority;
         this.status = taskInfo.status;
         this.numberOfTimes = taskInfo.numberOfTimes;
         this.repeatIntervalDays = taskInfo.repeatIntervalDays;
     }
-    
+
 }
