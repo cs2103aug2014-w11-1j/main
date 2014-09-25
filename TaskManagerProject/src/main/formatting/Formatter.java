36c059fea5a5f88eb56862b4f117fd9400a84af8
nathanajah <nathanajah@yahoo.com>
2014-09-25 17:05:25 +0800

    Added formatting for CannotExecuteCommandResponse

--- TaskManagerProject/src/main/formatting/Formatter.java
+++ TaskManagerProject/src/main/formatting/Formatter.java
@@ -1,5 +1,42 @@
 package main.formatting;
 
-public class Formatter {
+import java.util.ArrayList;
 
+import main.response.SearchResponse;
+import main.response.CannotExecuteCommandResponse;
+import main.response.Response;
+import data.taskinfo.TaskInfo;
+
+public class Formatter {
+    private final static String MESSAGE_CANNOT_EXECUTE_COMMAND =
+            "Sorry, Taskline could not execute your command.";
+    
+    private ArrayList<String> formatCannotExecuteCommand(
+            CannotExecuteCommandResponse response) {
+        ArrayList<String> formattedArray = new ArrayList<String>();
+        formattedArray.add(MESSAGE_CANNOT_EXECUTE_COMMAND);
+        return formattedArray;
+    }
+    
+    private String convertToString(ArrayList<String> lines) {
+        StringBuilder resultBuilder = new StringBuilder("");
+        for (String line : lines) {
+            resultBuilder.append(line);
+            resultBuilder.append(System.lineSeparator());
+        }
+        return resultBuilder.toString();
+    }
+    
+    public String format(Response response) {
+        ArrayList<String> lines;
+        switch(response.getType()){
+            case CANNOT_EXECUTE_COMMAND :
+                CannotExecuteCommandResponse castedResponse = 
+                    (CannotExecuteCommandResponse)response;
+                lines = formatCannotExecuteCommand(castedResponse);
+            default : 
+                lines = new ArrayList<String>();
+        }
+        return convertToString(lines);
+    }
 }
