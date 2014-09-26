36a7f7049b176ff3c42fcc963406bcacd71a3c61
nathanajah <nathanajah@yahoo.com>
2014-09-26 23:50:17 +0800

    Removed MESSAGE from Response.Type

--- TaskManagerProject/src/main/response/Response.java
+++ TaskManagerProject/src/main/response/Response.java
@@ -4,7 +4,6 @@ public interface Response {
 
     public enum Type {
         EMPTY_STRING,   // print no output
-        MESSAGE,        // print only displayed message
         ENUM_MESSAGE,    // print the message type stored in the enum.
         SEARCH_RESULTS,
         EDITMODE,
