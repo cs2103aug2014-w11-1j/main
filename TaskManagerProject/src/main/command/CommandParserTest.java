e9d7a3e8b49f02bccd8b244905f6f28c2e5c06a4
YJSoh <soh.you.jun@gmail.com>
2014-09-30 11:07:07 +0800

    Added more datetime test variables and refactored current ones. Added datetime test cases.

--- TaskManagerProject/src/main/command/CommandParserTest.java
+++ TaskManagerProject/src/main/command/CommandParserTest.java
@@ -34,12 +34,15 @@ public class CommandParserTest {
     private static final String PRI_HIGH = "+HIGH";
 
     private enum TimeTest {
-        ABS_12_HOURS_LONG  ("3:46 PM"),
-        ABS_12_HOURS_SHORT ("3 PM"),
-        ABS_24_HOURS_LONG  ("15:46"),
-        REL_PLUS_HOURS     ("+3h"),
-        REL_MINUS_HOURS    ("-3h"),
-        REL_NOW            ("now");
+        ABS_12_HOURS_LONG_1  ("9:46 AM"),
+        ABS_12_HOURS_SHORT_1 ("9 AM"),
+        ABS_24_HOURS_LONG_1  ("9:46"),
+        ABS_12_HOURS_LONG_2  ("3:16 PM"),
+        ABS_12_HOURS_SHORT_2 ("3 PM"),
+        ABS_24_HOURS_LONG_2  ("15:16"),
+        REL_PLUS_HOURS       ("+3h"),
+        REL_MINUS_HOURS      ("-3h"),
+        REL_NOW              ("now");
 
         private final String type;
         TimeTest(String type) {
@@ -47,12 +50,20 @@ public class CommandParserTest {
         }
     }
 
+    private static final LocalTime TIME_1 = LocalTime.of(9, 46);
+    private static final LocalTime TIME_2 = LocalTime.of(15, 16);
+
     private enum DateTest {
-        ABS_D_MMMM        ("24 August"),
-        ABS_DMMM          ("24Aug"),
-        ABS_DMMMY         ("24Aug2014"),
-        ABS_DMMMYY        ("24Aug14"),
-        ABS_D_M_YY        ("24/8/14"),
+        ABS_D_MMMM_1      ("24 August"),
+        ABS_DMMM_1        ("24Aug"),
+        ABS_DMMMY_1       ("24Aug2014"),
+        ABS_DMMMYY_1      ("24Aug14"),
+        ABS_D_M_YY_1      ("24/8/14"),
+        ABS_D_MMMM_2      ("13 september"),
+        ABS_DMMM_2        ("13sep"),
+        ABS_DMMMY_2       ("13sep2014"),
+        ABS_DMMMYY_2      ("13sep14"),
+        ABS_D_M_YY_2      ("13/9/14"),
         REL_WEEKDAY_SHORT ("tues"),
         REL_WEEKDAY_LONG  ("Tuesday"),
         REL_TODAY         ("today"),
@@ -68,15 +79,19 @@ public class CommandParserTest {
         }
     }
 
+    private static final LocalDate DATE_1 = LocalDate.of(2014, 8, 24);
+    private static final LocalDate DATE_2 = LocalDate.of(2014, 9, 13);
+
     @Test
     public void testParseTask() {
+        // terms, ignored terms, 2 tags, 1 priority, 1 date, 1 time
         String test1 = mergeStrings(new String[]{TERM_1, TERM_IGNORE_1, TERM_2,
                 TAG+TERM_7, TERM_3, TAG+TERM_4, PRI_HIGH,
-                TimeTest.ABS_12_HOURS_LONG.type, DateTest.ABS_DMMMY.type});
+                TimeTest.ABS_12_HOURS_LONG_1.type, DateTest.ABS_DMMMY_1.type});
         TaskInfo t = CommandParser.parseTask(test1);
         assertEquals(t.name, test1);
-        assertEquals(t.endDate, LocalDate.of(2014, 8, 24));
-        assertEquals(t.endTime, LocalTime.of(15, 46));
+        assertEquals(t.endDate, DATE_1);
+        assertEquals(t.endTime, TIME_1);
         assertEquals(t.duration, Duration.ZERO);
         assertEquals(t.priority, Priority.HIGH);
         StringBuilder result = new StringBuilder();
@@ -88,7 +103,86 @@ public class CommandParserTest {
 
     @Test
     public void testParseDateTime() {
-        //CommandParser
+        TaskInfo t = new TaskInfo();
+        // nothing: null
+        String test1 = mergeStrings(new String[]{TERM_2});
+        CommandParser.parseDateTime(test1, t);
+        assertEquals(t.endDate, null);
+        assertEquals(t.endTime, null);
+        assertEquals(t.duration, null);
+
+        t = new TaskInfo();
+        // one date: null
+        String test2 = mergeStrings(new String[]{DateTest.ABS_D_M_YY_1.type});
+        CommandParser.parseDateTime(test2, t);
+        assertEquals(t.endDate, null);
+        assertEquals(t.endTime, null);
+        assertEquals(t.duration, null);
+
+        t = new TaskInfo();
+        // two dates: null
+        String test3 = mergeStrings(new String[]{DateTest.ABS_D_MMMM_1.type,
+                DateTest.ABS_D_MMMM_2.type});
+        CommandParser.parseDateTime(test3, t);
+        assertEquals(t.endDate, null);
+        assertEquals(t.endTime, null);
+        assertEquals(t.duration, null);
+
+        t = new TaskInfo();
+        // one time, zero date: next occurrence of time
+        String test4 = mergeStrings(new String[]{TimeTest.ABS_12_HOURS_LONG_1.type});
+        CommandParser.parseDateTime(test4, t);
+        if (LocalTime.now().isBefore(TIME_1)) {
+            assertEquals(t.endDate, LocalDate.now());
+        } else {
+            assertEquals(t.endDate, LocalDate.now().plusDays(1));
+        }
+        assertEquals(t.endTime, TIME_1);
+        assertEquals(t.duration, Duration.ZERO);
+
+        t = new TaskInfo();
+        // one time, one date: time on that date
+        String test5 = mergeStrings(new String[]{DateTest.ABS_DMMM_2.type,
+                TimeTest.ABS_24_HOURS_LONG_2.type});
+        CommandParser.parseDateTime(test5, t);
+        assertEquals(t.endDate, DATE_2);
+        assertEquals(t.endTime, TIME_2);
+        assertEquals(t.duration, Duration.ZERO);
+
+        t = new TaskInfo();
+        // one time, two date: null
+        String test6 = mergeStrings(new String[]{TimeTest.ABS_12_HOURS_LONG_2.type,
+                DateTest.ABS_D_MMMM_1.type, DateTest.ABS_DMMMYY_2.type});
+        CommandParser.parseDateTime(test6, t);
+        assertEquals(t.endDate, null);
+        assertEquals(t.endTime, null);
+        assertEquals(t.duration, null);
+
+        t = new TaskInfo();
+        // two time, zero date: next occurrence of first time
+        String test7 = mergeStrings(new String[]{TimeTest.ABS_12_HOURS_LONG_2.type,
+                TimeTest.ABS_12_HOURS_LONG_1.type});
+        CommandParser.parseDateTime(test7, t);
+        if (LocalTime.now().isBefore(TIME_2)) {
+            if (TIME_2.isAfter(TIME_1)) {
+                assertEquals(t.endDate, LocalDate.now().plusDays(1));
+                assertEquals(t.duration, Duration.between(TIME_2, TIME_1).plusDays(1));
+            } else {
+                assertEquals(t.endDate, LocalDate.now());
+                assertEquals(t.duration, Duration.between(TIME_2, TIME_1).plusDays(1));
+            }
+        } else {
+            if (TIME_2.isAfter(TIME_1)) {
+                assertEquals(t.endDate, LocalDate.now().plusDays(2));
+                assertEquals(t.duration, Duration.between(TIME_2, TIME_1).plusDays(1));
+            } else {
+                assertEquals(t.endDate, LocalDate.now().plusDays(1));
+                assertEquals(t.duration, Duration.between(TIME_2, TIME_1).plusDays(1));
+            }
+        }
+        assertEquals(t.endTime, TIME_1);
+
+
     }
 
     @Test
