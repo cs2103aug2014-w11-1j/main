659656e7c1d0591645e743f90bdc6159ee73c8d7
Bruce <dingming_1994@126.com>
2014-10-07 10:17:58 +0800

    dummy commit

--- TaskManagerProject/src/manager/StateManagerTest.java
+++ TaskManagerProject/src/manager/StateManagerTest.java
@@ -1,5 +1,30 @@
 package manager;
 
+import javax.swing.undo.UndoManager;
+
+import manager.result.AddResult;
+import manager.result.Result;
+
+import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import data.taskinfo.TaskInfo;
+
 public class StateManagerTest {
+	
+		
+	@Test
+	public void addTest(){
+		TaskInfo testTaskInfo1 = new TaskInfo();
+		testTaskInfo1.name = "Test1";
+		testTaskInfo1.details = "No details";
+		TeskInfo 
+		
+		StateManagerWithoutFileForTest stateManager =
+				new StateManagerWithoutFileForTest(null, null);
+		AddResult addResult = new AddResult(Result.Type.ADD_SUCCESS, testTaskInfo1, taskId)
+	}
+	
+	
 
 }
