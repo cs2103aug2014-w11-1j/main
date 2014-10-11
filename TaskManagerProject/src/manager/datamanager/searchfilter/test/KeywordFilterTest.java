962f34bf320991b601d8cbba0727b1e44fd1d8d7
nathanajah <nathanajah@yahoo.com>
2014-10-11 18:06:26 +0800

    Added a few more test to the KeywordFilter

--- TaskManagerProject/src/manager/datamanager/searchfilter/test/KeywordFilterTest.java
+++ TaskManagerProject/src/manager/datamanager/searchfilter/test/KeywordFilterTest.java
@@ -41,10 +41,10 @@ public class KeywordFilterTest {
     }
     
     @Test
-    public void testSingleWordNameFalse() {
+    public void testSingleDetailsNameFalse() {
         TaskInfo testedTask = TaskInfo.create();
-        testedTask.name = "I am going to test this task";
-        testedTask.details = null;
+        testedTask.name = null;
+        testedTask.details = "I am going to test this task";
         String[] words = new String[]{"doesntexist"};
         KeywordFilter filter = new KeywordFilter(words);
         assertFalse(filter.filter(testedTask));
@@ -86,6 +86,32 @@ public class KeywordFilterTest {
         testedTask.name = "I am going to test this task";
         testedTask.details = null;
         String[] words = new String[]{"TeSt"};
+        KeywordFilter filter = new KeywordFilter(words);
+        assertTrue(filter.filter(testedTask));
+    }
+    
+    @Test
+    public void testTab() {
+        TaskInfo testedTask = TaskInfo.create();
+        testedTask.name = "abcd\ttest\tjaksldjf";
+        String[] words = new String[]{"test"};
+        KeywordFilter filter = new KeywordFilter(words);
+        assertTrue(filter.filter(testedTask));
+    }
+    
+    @Test
+    public void testNewline() {
+        TaskInfo testedTask = TaskInfo.create();
+        testedTask.name = "hahaha\ntest\nhahaha";
+        String[] words = new String[]{"test"};
+        KeywordFilter filter = new KeywordFilter(words);
+        assertTrue(filter.filter(testedTask));
+    }
+    @Test
+    public void testPunctuation( ){
+        TaskInfo testedTask = TaskInfo.create();
+        testedTask.name = "hahaha$%^$%^$!@#test^$&@#$hahaha";
+        String[] words = new String[]{"test"};
         KeywordFilter filter = new KeywordFilter(words);
         assertTrue(filter.filter(testedTask));
     }
