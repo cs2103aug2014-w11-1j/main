1eeb1233d12978cb4baf7633ad860a9a842f6ee6
Bruce <dingming_1994@126.com>
2014-10-17 12:47:04 +0800

    change getTaskStartDate method

---
+++ TaskManagerProject/src/manager/datamanager/test/FreeTimeSlotManagerTest.java
@@ -0,0 +1,15 @@
+package manager.datamanager.test;
+
+import data.TaskData;
+
+public class FreeTimeSlotManagerTest {
+	
+	public static void main(String[] args){
+		
+		TaskData taskData = new TaskData();
+	}
+	
+	
+	private static void addTask(String name)
+
+}
