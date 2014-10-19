8448818daa2670a50df06b4358bc02756ad3b4e6
YJSoh <soh.you.jun@gmail.com>
2014-10-19 16:51:31 +0800

    Added public getters to DateTimePair.

--- TaskManagerProject/src/main/command/parser/DateTimePair.java
+++ TaskManagerProject/src/main/command/parser/DateTimePair.java
@@ -58,6 +58,22 @@ public class DateTimePair {
         return count == max;
     }
 
+    public LocalDate getFirstDate() {
+        return firstDate;
+    }
+
+    public LocalTime getFirstTime() {
+        return firstTime;
+    }
+
+    public LocalDate getSecondDate() {
+        return secondDate;
+    }
+
+    public LocalTime getSecondTime() {
+        return secondTime;
+    }
+
     public int getNumOfDates() {
         int first = firstDate == null ? 0 : 1;
         int second = secondDate == null ? 0 : 1;
