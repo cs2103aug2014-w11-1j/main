cf7fb943c0424054cdc6f0ec4cfd88cc09c08f49
Bruce <dingming_1994@126.com>
2014-10-28 11:44:38 +0800

    commit for freetime manager ( ongoing)

--- TaskManagerProject/src/manager/datamanager/freetimemanager/FreeTimeSearchManager.java
+++ TaskManagerProject/src/manager/datamanager/freetimemanager/FreeTimeSearchManager.java
@@ -1,17 +1,46 @@
 package manager.datamanager.freetimemanager;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
-import data.TaskData;
 import manager.datamanager.AbstractManager;
+import data.TaskData;
+import data.taskinfo.TaskInfo;
 
 public class FreeTimeSearchManager extends AbstractManager {
 	
+	
+	private ArrayList<TaskInfo> taskList;
+	private ArrayList<Interval> freeTimeList;
+	//private final LocalTime DAY_START_TIME
+	
 	public FreeTimeSearchManager(TaskData taskData) {
 		super(taskData);
-		// TODO Auto-generated constructor stub
 	}
-
-	private ArrayList<Interval> freeTimeList;
 	
+	private Interval getTaskTimeOnDate(TaskInfo taskInfo, LocalDate date){
+		
+		
+		if (taskInfo.getEndDate() == null){ // floating task
+			return null;
+		} else if (taskInfo.getEndTime() == null){ // no time task
+			return null;
+		} else if (taskInfo.getEndDate().isBefore(date)){ // past task
+			return null;
+		} else if (taskInfo.getStartDate() != null){
+			if (taskInfo.getStartDate().isAfter(date)){ // future task
+				return null;
+			}
+		} 
+		if (taskInfo.getEndDate().isEqual(date)){ // end that day
+			if (taskInfo.getStartDate() == null){
+				//
+			} else if (taskInfo.getStartDate().equals(date)){
+				return new Interval(taskInfo.getStartTime(), taskInfo.getEndTime());
+			} else if (taskInfo.getStartDate().isBefore(date)){
+			//	return new Interval(startTime, endTime)
+			}
+		}
+	}
 }
