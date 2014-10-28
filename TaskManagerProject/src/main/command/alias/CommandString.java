8e852032a5b0e451382654f3ba65fa9293aa761e
YJSoh <soh.you.jun@gmail.com>
2014-10-28 20:19:53 +0800

    Added CommandString class.

--- TaskManagerProject/src/main/command/alias/CommandString.java
+++ TaskManagerProject/src/main/command/alias/CommandString.java
@@ -1,9 +1,33 @@
 package main.command.alias;
 
 public class CommandString {
+    private AliasData.CommandType type;
+    private String alias;
 
-    public CommandString(String trim) {
-        // TODO Auto-generated constructor stub
+    public CommandString(AliasManager aliasManager, String cmdString) {
+        int delimIdx = cmdString.indexOf(' ');
+        if (delimIdx == -1) {
+            type = aliasManager.getReservedCommand(cmdString);
+            if (type == null) {
+                alias = cmdString;
+            } else {
+                alias = "";
+            }
+        } else {
+            type = aliasManager.getReservedCommand(cmdString.substring(0, delimIdx));
+            if (type == null) {
+                alias = cmdString;
+            } else {
+                alias = cmdString.substring(delimIdx + 1);
+            }
+        }
     }
 
+    public AliasData.CommandType getType() {
+        return type;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
 }
